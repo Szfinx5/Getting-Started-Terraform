cb7dd4dfd73cb7d1126a3d3b4e7f02a9e76d6d6a
ghavasi <g.havasi@outlook.com>
2022-09-12 14:55:41 +0100

    Added S3 bucket and policy

--- globo_web_app/loadbalancer.tf
+++ globo_web_app/loadbalancer.tf
@@ -1,3 +1,6 @@
+## aws_elb_service_account
+data "aws_elb_service_account" "root" {}
+
 ## aws_loadbalancer
 resource "aws_lb" "nginx" {
   name               = "globo-web-alb"
@@ -8,6 +11,11 @@ resource "aws_lb" "nginx" {
 
   enable_deletion_protection = false
 
+  access_logs {
+    bucket  = aws_s3_bucket.web_bucket.bucket
+    prefix  = "alb-logs"
+    enabled = true
+  }
 
   tags = local.common_tags
 }
